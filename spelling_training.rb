4c3b3f23f6bcf4a5a35c4a773a4835ddc1170ed9
Lucas Caton <lucascaton@gmail.com>
2015-03-27 15:54:07 +1000

    Adding numbers

--- spelling_training.rb
+++ spelling_training.rb
@@ -1,15 +1,28 @@
 system('clear')
 
+print "Type the kind of game you want to play (1 - words, 2 - numbers): "
+kind = gets.chomp.to_i
+
+unless [1, 2].include?(kind)
+  puts 'Invalid kind. Words will be used...'
+  kind = 1
+end
+
 print "Type the level you want to play (1~3): "
 level = gets.chomp.to_i
 
 unless [1, 2, 3].include?(level)
-  puts 'Invalid level. Using level 1 instead...'
+  puts 'Invalid level. Level 1 will be used...'
   level = 1
-  sleep 2
 end
 
-words = File.read('/usr/share/dict/words').split("\n")
+words = case kind
+  when 1 then File.read('/usr/share/dict/words').split("\n")
+  when 2 then 1000.times.map { 8.times.map { (0..9).to_a.sample }.join }
+end
+
+print "\nPress ENTER to start..."
+gets
 
 loop do
   system('clear')
